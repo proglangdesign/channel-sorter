f172a4bfc23a886c63f886a85ba761c4f4a94af0
Timon Paßlick <timon.passlick@gmx.de>
2019-10-15 15:13:33 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,28 @@ use serenity::{
     prelude::*,
 };
 
+//token is in gitignore so that it doesn't get leaked
+const TOKEN: &str = include_str!("bot-token.txt");
+const ACTIVE_CATEGORY: ChannelId = ChannelId(622797326553186365);
+const INACTIVE_CATEGORY: ChannelId = ChannelId(622797326553186367);
+const GUILD: GuildId = GuildId(622797326553186364);
+
+fn main() {
+    let mut client = Client::new(TOKEN, Handler).expect("Err creating client");
+
+    if let Err(why) = client.start() {
+        println!("Client error: {:?}", why);
+    }
+}
+
 struct Handler;
 
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, _msg: Message) {
-        const ACTIVE_CATEGORY_ID: u64 = 622797326553186365;
-        const INACTIVE_CATEGORY_ID: u64 = 622797326553186367;
-        let guild: GuildId = GuildId(622797326553186364);
-        let mut channels = guild.channels(&ctx).expect("Err getting channels");
+        let mut channels = GUILD.channels(&ctx).expect("Err getting channels");
         let relevant_channels = channels.iter_mut().filter_map(|(_id, guild_channel)| {
             match guild_channel.category_id {
-                Some(id) if id == ACTIVE_CATEGORY_ID || id == INACTIVE_CATEGORY_ID => {
+                Some(category) if category == ACTIVE_CATEGORY || category == INACTIVE_CATEGORY => {
                     Some(guild_channel)
                 }
                 _ => None,
@@ -25,7 +36,7 @@ impl EventHandler for Handler {
         });
         for (cnt, channel) in relevant_channels.enumerate() {
             println!("{}", cnt);
-            let new_category_id = match channel
+            let new_category = match channel
                 .messages(&ctx, |get_messages| get_messages.limit(1))
                 .expect("Err getting latest message in channel, even if it didn't exist")
                 .get(0)
@@ -37,27 +48,14 @@ impl EventHandler for Handler {
                         Utc::now() - timestamp_utc < Duration::days(30 * 2)
                     } =>
                 {
-                    ACTIVE_CATEGORY_ID
+                    ACTIVE_CATEGORY
                 }
-                _ => INACTIVE_CATEGORY_ID,
+                _ => INACTIVE_CATEGORY,
             };
-            if new_category_id == channel.category_id.unwrap_or(channel.id).0 {
+            if new_category == channel.category_id.unwrap() {
                 continue;
             }
-            let _ = channel.edit(&ctx, |edit_channel| {
-                edit_channel.category(ChannelId(new_category_id))
-            });
+            let _ = channel.edit(&ctx, |edit_channel| edit_channel.category(new_category));
         }
-    }
-}
-
-fn main() {
-    //token is in gitignore so that it doesn't get leaked
-    const TOKEN: &str = include_str!("bot-token.txt");
-
-    let mut client = Client::new(TOKEN, Handler).expect("Err creating client");
-
-    if let Err(why) = client.start() {
-        println!("Client error: {:?}", why);
     }
 }
